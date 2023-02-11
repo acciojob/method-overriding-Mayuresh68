1e3533ca84f4d2f5629ac9480b825cfc1a35a531
Mayuresh <mayureshpatil068@gmail.com>
2023-02-11 16:16:04 +0530

    overriding assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,21 @@
 package com.driver;
+import java.util.*;
 
 public class Main {
-  public static class A{
-      public String meth(){
-          return "Invoking method from class A";
-      }
-  }
-  public static class B extends  A {
-      public String meth(){
-          return "Method is overridden in Extendend class B";
-      }
-  }
-
+    public static class A{
+        public String meth(){
+            String st= "Invoking method from class A";
+            return st;
+        }
+    }
+    public static class B extends  A{
+        public String meth(){
+            String st= "Method is overridden in Extendend class B";
+            return st;
+        }
+    }
     public static void main(String[] args) {
-        B b = new B();
-        b.meth();
+        B b= new B();
         b.meth();
     }
-
 }
